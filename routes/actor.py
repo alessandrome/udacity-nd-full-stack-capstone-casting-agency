f49a762ebd7221cb82cb5143fbd33da2c5e4df02
alessandrome <mesti.alessandro.git@gmail.com>
2020-03-06 00:17:17 +0100

    Prepared API endpoints

--- routes/actor.py
+++ routes/actor.py
@@ -2,9 +2,38 @@ from flask import Blueprint, request, jsonify
 from models import db, Movie, Actor
 import datetime
 import utils
-import random
 import auth
 import errors
 
+
 actor_blueprint = Blueprint('actor', __name__)
 
+
+@auth.requires_auth('create:actor')
+@actor_blueprint.route('/actors', methods=['POST'])
+def create_actor():
+    pass
+
+
+@auth.requires_auth('read:actor')
+@actor_blueprint.route('/actors')
+def get_actors():
+    pass
+
+
+@auth.requires_auth('read:actor')
+@actor_blueprint.route('/actors/<int:actor_id>')
+def get_actor(actor_id):
+    pass
+
+
+@auth.requires_auth('update:actor')
+@actor_blueprint.route('/actors/<int:actor_id>', methods=['PATCH'])
+def patch_actor(actor_id):
+    pass
+
+
+@auth.requires_auth('delete:actor')
+@actor_blueprint.route('/actors/<int:actor_id>', methods=['PATCH'])
+def delete_actor(actor_id):
+    pass
--- routes/movie.py
+++ routes/movie.py
@@ -2,8 +2,38 @@ from flask import Blueprint, request, jsonify
 from models import db, Movie, Actor
 import datetime
 import utils
-import random
 import auth
 import errors
 
+
 movie_blueprint = Blueprint('movie', __name__)
+
+
+@auth.requires_auth('create:movie')
+@movie_blueprint.route('/movies', methods=['POST'])
+def create_movie():
+    pass
+
+
+@auth.requires_auth('read:movie')
+@movie_blueprint.route('/movies')
+def get_movies():
+    pass
+
+
+@auth.requires_auth('read:movie')
+@movie_blueprint.route('/movies/<int:movie_id>')
+def get_movie(movie_id):
+    pass
+
+
+@auth.requires_auth('update:movie')
+@movie_blueprint.route('/movies/<int:movie_id>', methods=['PATCH'])
+def patch_movie(movie_id):
+    pass
+
+
+@auth.requires_auth('delete:movie')
+@movie_blueprint.route('/movies/<int:movie_id>', methods=['PATCH'])
+def delete_movie(movie_id):
+    pass
