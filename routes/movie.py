001fe22dd4f219656f4db95681b11fbff2508dc4
alessandrome <mesti.alessandro.git@gmail.com>
2020-03-06 20:04:13 +0100

    Add movie APIs

--- routes/movie.py
+++ routes/movie.py
@@ -5,35 +5,71 @@ import utils
 import auth
 import errors
 
-
 movie_blueprint = Blueprint('movie', __name__)
 
 
 @auth.requires_auth('create:movie')
 @movie_blueprint.route('/movies', methods=['POST'])
 def create_movie():
-    pass
+    data = request.json
+    movie = Movie()
+    movie.title = data['title']
+    movie.release_date = data['release_date']
+    movie.insert()
+    return jsonify(movie.long())
 
 
 @auth.requires_auth('read:movie')
 @movie_blueprint.route('/movies')
 def get_movies():
-    pass
+    max_per_page = 50
+    page = request.args.get('page', 1, type=int)
+    per_page = request.args.get('perPage', 20, type=int)
+    q = Movie.query
+    search_term = request.args.get('search_term', None, str)
+    if search_term:
+        q = q.filter(Movie.title.ilike(search_term))
+    return_movies = []
+    pagination = q.paginate(page, per_page, max_per_page)
+    for movie in pagination.items:
+        return_movies.append(movie.short())
+    return_data = {
+        'items': return_movies,
+        'total_items': pagination.total,
+        'page': pagination.page,
+        'pages': pagination.pages,
+    }
+    return jsonify(return_data)
 
 
 @auth.requires_auth('read:movie')
 @movie_blueprint.route('/movies/<int:movie_id>')
 def get_movie(movie_id):
-    pass
+    movie = Movie.query.filter(Movie.id == movie_id).first()
+    if not movie:
+        return errors.not_found_error('Movie not found')
+    return jsonify(movie.long())
 
 
 @auth.requires_auth('update:movie')
 @movie_blueprint.route('/movies/<int:movie_id>', methods=['PATCH'])
 def patch_movie(movie_id):
-    pass
+    movie = Movie.query.filter(Movie.id == movie_id).first()
+    if not movie:
+        return errors.not_found_error('Movie not found')
+    data = request.json
+    if 'title' in data:
+        movie.title = data['title']
+    if 'release_date' in data:
+        movie.release_date = data['release_date']
+    return jsonify(movie.long())
 
 
 @auth.requires_auth('delete:movie')
 @movie_blueprint.route('/movies/<int:movie_id>', methods=['PATCH'])
 def delete_movie(movie_id):
-    pass
+    movie = Movie.query.filter(Movie.id == movie_id).first()
+    if not movie:
+        return errors.not_found_error('Movie not found')
+    movie.delete()
+    return '', 204
